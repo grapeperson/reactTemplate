320255f1b278b10f212af25977eec7f7528bb896
grapeperson <grapeperson@inbox.ru>
2018-07-09 19:30:53 +0300

    add/html_plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -17,5 +18,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })
+]
 };
